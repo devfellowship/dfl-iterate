2375ccb216590e217f128c488001d85ed90efb62
RaulAl3n <raulscalassara@gmail.com>
2026-06-20 14:37:42 -0300

    feat: useLeaderBoardTable implemented and leaderBoard enhanced

--- src/services/leaderboard.service.ts
+++ src/services/leaderboard.service.ts
@@ -1,12 +1,12 @@
 import { leaderboardData } from '@/test-utils/leaderboard.dummy';
 import { simulateNetworkDelay } from '@/services/activities.service';
 
-export async function getLeaderboard() {
+export async function getLeaderboard(limit) {
     await simulateNetworkDelay();
 
     if (!leaderboardData || leaderboardData.length === 0) {
         throw new Error('Leaderboard data is empty or not available.');
     }
 
-    return Promise.resolve(leaderboardData);
+    return Promise.resolve(leaderboardData.slice(0, limit));
 };
