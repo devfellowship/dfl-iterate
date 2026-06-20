bb0bd86fa8282737fd3eb3fe5b46b4c43cd46dc8
RaulAl3n <raulscalassara@gmail.com>
2026-06-20 14:17:57 -0300

    feat: add leaderboard functionality with entry interface and dummy data

---
+++ src/test-utils/leaderboard.dummy.ts
@@ -0,0 +1,44 @@
+import { LeaderboardEntry } from '@/types';
+
+export const leaderboardData: LeaderboardEntry[] = [
+  {
+    "rank": 1,
+    "userId": "user-42",
+    "displayName": "Carlos Dev",
+    "avatarUrl": "https://i.pravatar.cc/64?u=user-42",
+    "totalXp": 3200,
+    "isCurrentUser": false
+  },
+  {
+    "rank": 2,
+    "userId": "user-17",
+    "displayName": "Beatriz Code",
+    "avatarUrl": "https://i.pravatar.cc/64?u=user-17",
+    "totalXp": 2850,
+    "isCurrentUser": false
+  },
+  {
+    "rank": 3,
+    "userId": "user-1",
+    "displayName": "Ana Fellow",
+    "avatarUrl": "https://i.pravatar.cc/64?u=user-1",
+    "totalXp": 1240,
+    "isCurrentUser": true
+  },
+  {
+    "rank": 4,
+    "userId": "user-88",
+    "displayName": "Diego React",
+    "avatarUrl": "https://i.pravatar.cc/64?u=user-88",
+    "totalXp": 980,
+    "isCurrentUser": false
+  },
+  {
+    "rank": 5,
+    "userId": "user-33",
+    "displayName": "Elena TS",
+    "avatarUrl": "https://i.pravatar.cc/64?u=user-33",
+    "totalXp": 750,
+    "isCurrentUser": false
+  }
+];
